750cb47b2ac74570243fe29f2fb9b38579e3d49b
yusufyp <yusuf.yudhistirazhy@gmail.com>
2021-11-08 13:31:05 +0700

    add axios and firebase realtime database

--- pages/admin/new-post/index.vue
+++ pages/admin/new-post/index.vue
@@ -1,19 +1,29 @@
 <template>
   <div class="admin-new-post-page">
     <section class="new-post-form">
-        <AdminPostForm/>
+        <AdminPostForm @submit="onSubmitted"/>
     </section>
   </div>
 </template>
 
 <script>
 import AdminPostFrom from '@/components/Admin/AdminPostForm';
+import axios from 'axios';
 
 export default {
     layout: 'admin',
     components : {
         AdminPostFrom,
-    }
+    },
+    
+    methods: {
+      onSubmitted(postData){
+        axios.post('https://nuxt-blog-3c79d-default-rtdb.asia-southeast1.firebasedatabase.app/posts.json', postData)
+          .then(result => {
+            console.log(result);
+          }).catch(error => console.log(error));
+      }
+    },
     
 }
 </script>
